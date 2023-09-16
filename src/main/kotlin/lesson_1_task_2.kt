283ff3d8bbec926436afa4f67254dcfef2ca596f
Viktor Mikhaylov <mikhaylovvn1101@gmail.com>
2023-09-16 19:33:59 +0500

    KS-1-2 Completed task 2

---
+++ src/main/kotlin/lesson_1_task_2.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    var numberOfOrders: Int = 75
+    val textOfGratitude: String = "thank you for choosing our product"
+
+    println(numberOfOrders)
+    println(textOfGratitude)
+
+    var numberOfWorkers: Int = 2000
+//    println(numberOfWorkers)
+
+    numberOfWorkers -= 1
+    println(numberOfWorkers)
+}
