46a42c4a475d431710376bb0e9ca05a62186716b
Viktor Mikhaylov <mikhaylovvn1101@gmail.com>
2023-09-16 21:15:07 +0500

    KS-1-4 Completed task 4

---
+++ src/main/kotlin/lesson_1_task_4.kt
@@ -0,0 +1,17 @@
+fun main (){
+
+    val length: Long = 4086860000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println("Length: " + length)
+    println("Age: " + age)
+    println("Part of day: " + partOfDay)
+    println("Seconds: " + seconds)
+    println("Part of year: " + partOfYear)
+    println("Apogee: " + apogee)
+
+}
