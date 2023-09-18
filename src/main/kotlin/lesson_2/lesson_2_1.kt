ef412136bf2da17dbe9a9edab61692fee7db8f13
Viktor Mikhaylov <mikhaylovvn1101@gmail.com>
2023-09-18 21:24:34 +0500

    KS-2-1 completed task 1

---
+++ src/main/kotlin/lesson_2/lesson_2_1.kt
@@ -0,0 +1,11 @@
+package lesson_2
+
+fun main(){
+    val markStudent1: Double = 3.0
+    val markStudent2: Double = 4.0
+    val markStudent3: Double = 3.0
+    val markStudent4: Double = 5.0
+
+    val averageMark = (markStudent1 + markStudent2 + markStudent3 + markStudent4)/4
+    println(averageMark)
+}
