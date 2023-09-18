bc7ceb54d1682ff9faff409bd984639849dcc859
Viktor Mikhaylov <mikhaylovvn1101@gmail.com>
2023-09-18 21:12:02 +0500

    KS-1-5 applied ".format"

--- src/main/kotlin/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1_task_5.kt
@@ -5,10 +5,5 @@ fun main() {
     val reminderOfMinutes = minutes % 60
     val hours = minutes / 60
 
-//    println(reminderOfSeconds)
-//    println(minutes)
-//    println(reminderOfMinutes)
-//    println(hours)
-
-    println("0$hours:$reminderOfMinutes:0$reminderOfSeconds")
+    println("%02d:%02d:%02d".format(hours,reminderOfMinutes,reminderOfSeconds))
 }
