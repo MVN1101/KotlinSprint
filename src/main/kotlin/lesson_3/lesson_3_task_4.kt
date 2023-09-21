6ff74281e7805405e6f9f16af34c864e4ec56c0f
Viktor Mikhaylov <mikhaylovvn1101@gmail.com>
2023-09-21 19:44:24 +0500

    KS-3-4 Completed task 4

--- src/main/kotlin/lesson_3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -1,2 +1,20 @@
 package lesson_3
 
+fun main() {
+    var markMoveFrom = "E"
+    var numberMoveFrom = 2
+    var markMoveTo = "E"
+    var numberMoveTo = 4
+    var numberOfMoves = 1
+
+    println("$markMoveFrom$numberMoveFrom-$markMoveTo$numberMoveTo; $numberOfMoves")
+
+    markMoveFrom ="D"
+    numberMoveFrom = 2
+    markMoveTo = "D"
+    numberMoveTo = numberMoveFrom + 1
+    numberOfMoves ++
+
+    println("$markMoveFrom$numberMoveFrom-$markMoveTo$numberMoveTo; $numberOfMoves")
+
+}
