f42db99d66e9f932d885bce47b52ccb5b65cf27c
Viktor Mikhaylov <mikhaylovvn1101@gmail.com>
2023-09-21 05:50:57 +0500

    KS-3-2 Completed task 2

--- src/main/kotlin/lesson_3/lesson_3_task_2.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -1,2 +1,15 @@
 package lesson_3
 
+fun main() {
+
+    val name = "Татьяна"
+    var lastName = "Андреева"
+    val patronymic = "Сергеевна"
+    var age = 20
+
+    println("$lastName $name $patronymic, $age лет.")
+
+    age = 22
+    lastName = "Сидорова"
+    println("$lastName $name $patronymic, $age года.")
+}
