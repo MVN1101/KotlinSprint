d8f0a3f3b4cabd3cdeb35ca1388598f001401454
Viktor Mikhaylov <mikhaylovvn1101@gmail.com>
2023-09-21 05:35:39 +0500

    KS-3-1 Completed task 1

--- src/main/kotlin/lesson_3/lesson_3_task_1.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_1.kt
@@ -1,2 +1,11 @@
 package lesson_3
 
+fun main() {
+
+    val name = "Viktor"
+    var greeting = "Good morning"
+    println("$greeting, $name!")
+
+    greeting = "Good evening"
+    println("$greeting, $name!")
+}
