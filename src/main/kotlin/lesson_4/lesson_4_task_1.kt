ae3e02768e236f2114cc943d5bb643203fda706a
Viktor Mikhaylov <mikhaylovvn1101@gmail.com>
2023-09-24 14:02:41 +0500

    KS-4-2 completed task 2 (review)

--- src/main/kotlin/lesson_4/lesson_4_task_1.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_1.kt
@@ -1,1 +1,11 @@
 package lesson_4
+fun main() {
+
+    val numberOfTables = 13
+    val busyTablesTd = 13
+    val busyTablesTm= 9
+
+    println("Доступность столиков на сегодня: ${busyTablesTd < numberOfTables} \n"  +
+            "Доступность столиков на завтра: ${busyTablesTm < numberOfTables}")
+
+}
